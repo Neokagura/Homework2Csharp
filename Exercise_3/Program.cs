3b9dd3ac129d909f1c881c2bc7530be0b295c085
Anastasiia <buravlyova14@gmail.com>
2023-07-20 18:23:25 +0300

    yeah

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Add a number from 1 to 7");
+int number= Convert.ToInt32 (Console.ReadLine());
+
+if (number>5)
+{
+    Console.WriteLine("Yeah, you can sit on your ass");
+}
+else
+{
+    Console.WriteLine("Fuck, You need to go to work");
+}
